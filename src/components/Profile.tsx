79f16e299c04e30d2aece0a0abdb632a807f3e97
João Kleberson <joao.nunes@mps.com.br>
2021-02-27 15:36:45 -0300

    Parte 4 - Notificações ao concluir desafio

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/Profile.module.css";
 
 export function Profile(){
+
+    const { level } = useContext(ChallengesContext);
+
     return(
         <div className={styles.profileContainer}>
             <img src="https://lh3.googleusercontent.com/ogw/ADGmqu-VvF3iAulqGR4Y-nsveXUU2T92fKQEfXJkV0yYcA=s90-c-mo" alt="João Kleberson"/>
@@ -8,7 +13,7 @@ export function Profile(){
                 <strong>João Kleberson</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level"/>
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
